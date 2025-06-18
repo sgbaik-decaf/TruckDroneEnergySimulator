457fdc03b5b6b139331c3458620c77ce9b2e9555
Seung Gyu Baik <157436755+sgbaik-decaf@users.noreply.github.com>
2025-06-17 21:17:54 -0500

    Update addon_main.py

--- addon_main.py
+++ addon_main.py
@@ -1,36 +1,24 @@
-#_____________________[PACKAGE INFORMATION]__________________
+# https://github.com/sgbaik-decaf/TruckDroneEnergySimulator
 
 # Author:      Baik, Seung Gyu
-# Contact:     seunggyb@alumni.cmu.edu
-# Affiliation: Carnegie Mellon University
-
-# Accompanying Article:
-# Framework for modeling Energy Savings of Truck-drone Hybrid Deliveries in Arbitrary Cities
-
-
+# Contact:     sgbaik@utexas.edu
 
 #_________________________[DISCLAIMER]_______________________
 
-# This package is designed to be an "add-on" extension to the mFSTSP Solver.
-# The original source code by Murray & Raj (2020) is on Github.
+# This package is designed to be an "add-on" extension to the mFSTSP Solver by Murray & Raj (2020)
 # https://github.com/optimatorlab/mFSTSP
 
-
-
 #________________[DEPENDENCIES FOR THIS PROGRAM]_____________
-# 1. NUMPY         https://numpy.org/install/
-# 2. PANDAS        https://pandas.pydata.org/docs/getting_started/install.html
-# 3. NETWORKX      https://networkx.org/documentation/stable/install.html
-# 4. OSMNX         https://osmnx.readthedocs.io/en/stable/installation.html
-# 5. MATPLOTLIB    https://matplotlib.org/stable/install/index.html
-# 6. SEABORN       https://seaborn.pydata.org/installing.html
-# 7. MFSTSP SOLVER PACKAGE (THE WHOLE THING including GUROBIPY)
-# 8. "addon_OSMnxGeospatialSimulator.py",
-#    "addon_EnergyConsumptionCalculator.py",
-#    "addon_HaversineFunction.py",
-#    "addon_GreedyGroundTSP.py"
-
-
+# 1. Numpy         https://numpy.org/install/
+# 2. Pandas        https://pandas.pydata.org/docs/getting_started/install.html
+# 3. NetworkX      https://networkx.org/documentation/stable/install.html
+# 4. OSMnx         https://osmnx.readthedocs.io/en/stable/installation.html
+# 5. mFSTSP solver package (THE WHOLE THING including GUROBIPY)
+# "addon_OSMnxGeospatialSimulator.py",
+# "addon_EnergyConsumptionCalculator.py",
+# "addon_HaversineFunction.py",
+# "addon_GreedyGroundTSP.py"
+# "newmain.py"
 
 #____________________________________________________________
 # Load Dependencies
@@ -54,8 +42,6 @@ try:
     import networkx as nx
     from networkx.algorithms.approximation import greedy_tsp
     import osmnx as ox
-    import matplotlib.pyplot as plt
-    import seaborn as sns
     
     from newmain import *
     from parseCSV import *
@@ -94,33 +80,41 @@ print('_' * os.get_terminal_size().columns)
 
 print('SET PARAMETERS')
 
-print('\nEnter a city name and state name to analyze\n(e.g. "Pittsburgh, PA")')
+print('\nEnter a city name. \n(e.g. "Pittsburgh, PA")')
 city_input = input('>>')
 
-print('\nEnter an address for delivery depot location \n(e.g. "1723 Murray Ave, Pittsburgh, PA")')
+print('\nEnter an address for delivery depot location. \n(e.g. "4800 Forbes Ave, Pittsburgh, PA")')
 depot_input = input('>>')
 
-print('\nSpecify the size of you area of operations in km \n(Suggested: 5~15)')
+print('\nSpecify the size of your area of operations in kilometers. \n(Suggested: 5~15)')
 bbox_range = int(input('>>'))
 
-print('\nSet analysis mode\n[1]: Single Instance Simulation\n[2]: Monte Carlo Simulation')
-MonteCarloIndicator = int(input('>>'))-1
-
-print('\nSpecify city center parameter in & \n(Suggested: 0.3~0.7)')
+print('\nSpecify city center parameter. \n(Suggested: 0.3~0.8)')
 CityCenterParam = float(input('>>'))
 
+print('\nSet analysis mode. \n[1]: Single Instance Simulation\n[2]: Monte Carlo Simulation')
+MonteCarloIndicator = int(input('>>'))-1
+
 if MonteCarloIndicator == 1:
-    print('\nSet number of simulations to run \n(Suggested: 30-100)')
+    print('\nSet number of simulations to run. \n(Suggested: 50-100)')
     MonteCarloInstances = int(input('>>'))
+    print('\nSave results (energy consumption estimates) as .csv files?\n[1]: No\n[2]: Yes')
+    SaveEnergyUseResults = int(input('>>'))-1
+    print('\nSave nodes and solutions of each simulation instance as .csv files? \n[1]: No\n[2]: Yes')
+    SaveCSV = bool(int(input('>>'))-1)
 
 elif MonteCarloIndicator == 0:
     MonteCarloInstances = 1
-    print('\nDisplay intermediate data?\n[1]: No\n[2]: Yes')
+    print('\nSave results (energy consumption estimates) as .csv files?\n[1]: No\n[2]: Yes')
+    SaveEnergyUseResults = int(input('>>'))-1
+    print('\nDisplay intermediate data? \n[1]: No\n[2]: Yes')
     DisplayIntermediateDataFrame = bool(int(input('>>'))-1)
+    SaveCSV = False
 
-# Diesel truck depot to door = 4.29 kWh/mi = 2665.69 Wh/km (DoE 2020)
-# EV truck depot to door = 1 kWh/mi = 621.37 Wh/km (DoE 2020)
-# EV van depot to door = 0.56 kWh/mi = 347.97 Wh/km (DoE 2020)
+# ENERGY INTENSITY VALUES (US DOE 2020, depot to door delivery scenario)
+# Class 6 Diesel Truck: 4.29 kWh/mi = 2665.69 Wh/km
+# Class 6 EV Truck: 1 kWh/mi = 621.37 Wh/km
+# EV Van: 0.56 kWh/mi = 347.97 Wh/km
 
 BaselineInformation = {'Diesel Truck':2665.69, 'EV Truck':621.37, 'EV Van':347.97}
 
@@ -133,9 +127,14 @@ start_time = time.time()
 
 print('_' * os.get_terminal_size().columns)
 
-SavingsComparedToTruck = []
-SavingsComparedToEVTruck = []
-SavingsComparedToEVVan = []
+BaselineEnergyConsumption_DieselTruck = []
+BaselineEnergyConsumption_ElecTruck = []
+BaselineEnergyConsumption_ElecVan = []
+HybridEnergyConsumption_DieselTruck = []
+HybridEnergyConsumption_ElecTruck = []
+HybridEnergyConsumption_ElecVan = []
+
+GeneratedNodes = []
 
 for iterationno in range(MonteCarloInstances):
     int_start_time = time.time()
@@ -151,7 +150,7 @@ for iterationno in range(MonteCarloInstances):
         try:
             city,depot_xy,G,Gp,Gp_cust,Gp_cust_mini = BuildBoxGraph(city_input, depot_input, bbox_range, CityCenterParam)
             print(f'Graph Representation for "{city}" Generated')
-            ox.plot.plot_graph(G, node_size=1)
+            ox.plot.plot_graph(G, node_size=0.5)
             matrix_DF_copy = SimulateCustomers(city,depot_xy,G,Gp,Gp_cust,Gp_cust_mini)
             print('Customer Batch & Travel Matrix Generated')
         except:
@@ -159,27 +158,23 @@ for iterationno in range(MonteCarloInstances):
                 print('[ WARNING ] OpenStreetMap and/or OSMnx is not working as expected.\nThe program will try executing one last time.')
                 city,depot_xy,G,Gp,Gp_cust,Gp_cust_mini = BuildBoxGraph(city_input, depot_input, bbox_range, CityCenterParam)
                 print(f'Graph Representation for "{city}" Generated')
-                ox.plot.plot_graph(G, node_size=1)
+                ox.plot.plot_graph(G, node_size=0.5)
                 matrix_DF_copy = SimulateCustomers(city,depot_xy,G,Gp,Gp_cust,Gp_cust_mini)
                 print('Customer Batch & Travel Matrix Generated')
             except:
-                sys.exit('[ WARNING ] There was an error with OpenStreetMap and/or OSMnx. Common causes are:\n(1) Internet connection issue\n(2) Typo in query\n(3) Disconnected node')
+                sys.exit('[ EXCEPTION ] There was an error with OpenStreetMap and/or OSMnx. Common causes are: \n(1) OSM query input issue \n(2) Internet connection issue \n(3) Disconnected node')
     elif iterationno >= 1:
         try:
             matrix_DF_copy = SimulateCustomers(city,depot_xy,G,Gp,Gp_cust,Gp_cust_mini)
-            print('Customer Batch & Travel Matrix Generated')
         except:
             try:
                 matrix_DF_copy = SimulateCustomers(city,depot_xy,G,Gp,Gp_cust,Gp_cust_mini)
-                print('Customer Batch & Travel Matrix Generated')
             except:
                 try:
                     print('[ WARNING ] OpenStreetMap and/or OSMnx is not working as expected.\nThe program will try executing one last time.')
                     matrix_DF_copy = SimulateCustomers(city,depot_xy,G,Gp,Gp_cust,Gp_cust_mini)
-                    print('Customer Batch & Travel Matrix Generated')
                 except:
-                    warnings.warn('[ WARNING ] There was an error with OpenStreetMap and/or OSMnx. Intance this iteration will reiterate previous instance.')
-
+                    warnings.warn('[ EXCEPTION ] There was an error with OpenStreetMap and/or OSMnx. The program reiterate previous instance.')
 
 
     #____________________________________________________________
@@ -196,6 +191,10 @@ for iterationno in range(MonteCarloInstances):
     UAV_info = missionControl().return_UAV_INFO_DF()
     if MonteCarloIndicator == 0:
         print('Routing & Scheduling Plan Retrieved')
+        
+    if SaveCSV == True:
+        a.to_csv(f'Simulation_Instances_CSV/Customer_Nodes{iterationno}.csv')
+        b.to_csv(f'Simulation_Instances_CSV/mFSTSP_Solution{iterationno}.csv')
 
 
 
@@ -204,10 +203,11 @@ for iterationno in range(MonteCarloInstances):
 
     UAV_energy_DF = runDroneEnergyModule(sorties, UAV_info)
     Ground_energy_DF = runTruckEnergyModule(truck_tour, matrix_DF_copy, BaselineInformation)
-    UAV_energy_sum = round(UAV_energy_DF['energy_use(Wh)'].sum(), 2)
-    Truck_energy_sum = round(Ground_energy_DF['energy(Wh, Diesel Truck)'].sum(), 2)
-    EVTruck_energy_sum = round(Ground_energy_DF['energy(Wh, EV Truck)'].sum(), 2)
-    EVVan_energy_sum = round(Ground_energy_DF['energy(Wh, EV Van)'].sum(), 2)
+    UAV_energy_sum = round(UAV_energy_DF['energy_use(Wh)'].sum(), 4)
+    Truck_energy_sum = round(Ground_energy_DF['energy(Wh, Diesel Truck)'].sum(), 4)
+    EVTruck_energy_sum = round(Ground_energy_DF['energy(Wh, EV Truck)'].sum(), 4)
+    EVVan_energy_sum = round(Ground_energy_DF['energy(Wh, EV Van)'].sum(), 4)
+    
     if MonteCarloIndicator == 0:
         print('Energy Use for Hybrid Scenario Calculated')
 
@@ -232,39 +232,51 @@ for iterationno in range(MonteCarloInstances):
 
 
     #____________________________________________________________
-    # Compare hybrid scenario to baseline scenario
-
-    SavingsComparedToTruck.append(
-        round(
-              baseline_tsp_DF['energy(Wh, Diesel Truck)'][0]
-          -
-          (UAV_energy_sum + Truck_energy_sum),
-          2)
-        /1000)
-    SavingsComparedToEVTruck.append(
-        round(
-              baseline_tsp_DF['energy(Wh, EV Truck)'][0]
-          -
-          (UAV_energy_sum + EVTruck_energy_sum),
-          2)
-        /1000)
-    SavingsComparedToEVVan.append(
-        round(
-              baseline_tsp_DF['energy(Wh, EV Van)'][0]
-          -
-          (UAV_energy_sum + EVVan_energy_sum),
-          2)
-        /1000)
-
-    print('Energy Savings Calculated')
-
-
-
-    #____________________________________________________________
     # Complete this simulation instance and record its result
-
+    
+    BaselineEnergyConsumption_DieselTruck.append(
+        round(
+            baseline_tsp_DF['energy(Wh, Diesel Truck)'][0]
+            /1000,
+            4)
+        )
+    HybridEnergyConsumption_DieselTruck.append(
+        round(
+            (UAV_energy_sum + Truck_energy_sum)
+            /1000,
+            4)
+        )
+    
+    BaselineEnergyConsumption_ElecTruck.append(
+        round(
+            baseline_tsp_DF['energy(Wh, EV Truck)'][0]
+            /1000,
+            4)
+        )
+    HybridEnergyConsumption_ElecTruck.append(
+        round(
+            (UAV_energy_sum + EVTruck_energy_sum)
+            /1000,
+            4)
+        )
+    
+    BaselineEnergyConsumption_ElecVan.append(
+        round(
+            baseline_tsp_DF['energy(Wh, EV Van)'][0]
+            /1000,
+            4)
+        )
+    HybridEnergyConsumption_ElecVan.append(
+        round(
+            (UAV_energy_sum + EVVan_energy_sum)
+            /1000,
+            4)
+        )
+    
     int_end_time = time.time()
     int_time = round(int_end_time - int_start_time, 2)
+    print(f"Baseline(Diesel Truck): {round(baseline_tsp_DF['energy(Wh, Diesel Truck)'][0]/1000,4)} kWh")
+    print(f"Truck-Drone Hybrid(Diesel Truck): {round((UAV_energy_sum + Truck_energy_sum)/1000,4)} kWh")
     print('Simulation Result Noted')
     print(f'Instance #{iterationno+1} took {int_time} seconds\n')
 
@@ -275,7 +287,7 @@ for iterationno in range(MonteCarloInstances):
 
 if MonteCarloIndicator == 0 and DisplayIntermediateDataFrame == True:
     print('_' * os.get_terminal_size().columns)
-    print('[INTERMEDIATE DATAFRAME]')
+    print('[LAST MILE DILIVERY TOUR SOLUTION]')
     print(UAV_energy_DF)
 
 print('_' * os.get_terminal_size().columns)
@@ -284,88 +296,39 @@ print('[RESULT]')
 end_time = time.time()
 print("\nTotal elapsed time: ",round(end_time-start_time, 2), "seconds")
 
-print('\nEnergy Savings from Hybrid system compared to Diesel Truck (kWh):')
-print('MEAN:', round(np.average(SavingsComparedToTruck), 2))
-print('MEDIAN:', round(np.median(SavingsComparedToTruck), 2))
-print('SD:', round(np.std(SavingsComparedToTruck), 2))
-print('MIN~MAX:', round(np.min(SavingsComparedToTruck), 2), '~', round(np.max(SavingsComparedToTruck), 2))
+print('\nEnergy consumptions of Hybrid Delivery and Conventional Delivery Baseline - Diesel Truck (kWh):')
+print('BASELINE:', round(np.average(BaselineEnergyConsumption_DieselTruck), 4))
+print('HYBRID:', round(np.average(HybridEnergyConsumption_DieselTruck), 4))
 
-print('\nEnergy Savings from Hybrid system compared to EV Truck (kWh):')
-print('MEAN:', round(np.average(SavingsComparedToEVTruck), 2))
-print('MEDIAN:', round(np.median(SavingsComparedToEVTruck), 2))
-print('SD:', round(np.std(SavingsComparedToEVTruck), 2))
-print('MIN~MAX:', round(np.min(SavingsComparedToEVTruck), 2), '~', round(np.max(SavingsComparedToEVTruck), 2))
+print('\nEnergy consumptions of Hybrid Delivery and Conventional Delivery Baseline - EV Truck (kWh):')
+print('BASELINE:', round(np.average(BaselineEnergyConsumption_ElecTruck), 4))
+print('HYBRID:', round(np.average(HybridEnergyConsumption_ElecTruck), 4))
 
-print('\nEnergy Savings from Hybrid system compared to EV Van (kWh):')
-print('MEAN:', round(np.average(SavingsComparedToEVVan), 2))
-print('MEDIAN:', round(np.median(SavingsComparedToEVVan), 2))
-print('SD:', round(np.std(SavingsComparedToEVVan), 2))
-print('MIN~MAX:', round(np.min(SavingsComparedToEVVan), 2), '~', round(np.max(SavingsComparedToEVVan), 2))
-
+print('\nEnergy consumptions of Hybrid Delivery and Conventional Delivery Baseline - EV Van (kWh):')
+print('BASELINE:', round(np.average(BaselineEnergyConsumption_ElecVan), 4))
+print('HYBRID:', round(np.average(HybridEnergyConsumption_ElecVan), 4))
 
 
 #____________________________________________________________
-# Visualize Simulation Result
+# Save Simulation Result
 
-print('\nPlot and save results?\n[1]: No\n[2]: Yes')
-viz = int(input('>>'))-1
 
-if viz == 1:
+
+if SaveEnergyUseResults == 1:
     MC_sim_output_DF = pd.DataFrame(
         {
-            'vs Truck' : SavingsComparedToTruck,
-            'vs EV Truck' : SavingsComparedToEVTruck,
-            'vs EV Van' : SavingsComparedToEVVan
+            'Baseline (Diesel Truck)' : BaselineEnergyConsumption_DieselTruck,
+            'Hybrid (Diesel Truck)' : HybridEnergyConsumption_DieselTruck,
+            'Baseline (EV Truck)' : BaselineEnergyConsumption_ElecTruck,
+            'Hybrid (EV Truck)' : HybridEnergyConsumption_ElecTruck,
+            'Baseline (EV Van)' : BaselineEnergyConsumption_ElecVan,
+            'Hybrid (EV Van)' : HybridEnergyConsumption_ElecVan
         }
-    )
+    ) 
     
-    sns.catplot(
-        data = pd.melt(MC_sim_output_DF),
-        x = 'variable',
-        hue = 'variable', legend = False,
-        y = 'value',
-        palette = ['royalblue', 'springgreen', 'orange']
-        )
-    plt.subplots_adjust(top = 0.9)
-    plt.suptitle(f'Energy Savings for {city_input} (kWh/tour)')
-    plt.ylabel('')
-    plt.savefig('output_datapoints.png', dpi=300)
-    plt.show()
-    plt.close()
-    
-    fig, axs = plt.subplots(1, 3, figsize=(5, 3))
-    sns.boxplot(
-        data = MC_sim_output_DF,
-        y = 'vs Truck',
-        notch = True,
-        color = 'royalblue',
-        medianprops={"color": "black", "linewidth": 1},
-        ax = axs[0])
-    sns.boxplot(
-        data = MC_sim_output_DF,
-        y = 'vs EV Truck',
-        notch = True,
-        color = 'springgreen',
-        medianprops={"color": "black", "linewidth": 1},
-        ax = axs[1])
-    sns.boxplot(
-        data = MC_sim_output_DF,
-        y = 'vs EV Van',
-        notch = True,
-        color = 'orange',
-        medianprops={"color": "black", "linewidth": 1},
-        ax = axs[2])
-    
-    plt.tight_layout()
-    plt.subplots_adjust(top = 0.9)
-    plt.suptitle(f'Energy Savings for {city_input.upper()} (kWh/tour)')
-    plt.savefig('output_adjusted.png', dpi=300)
-    plt.show()
-    plt.close()
-    
-    MC_sim_output_DF.to_csv('output_DataFrame.csv', index=False)
+    MC_sim_output_DF.to_csv('Results/output_DataFrame.csv', index=False)
 
-    print('Predicted energy savings saved as "output_DataFrame.csv".')
+    print('Predicted energy consumption saved as "Results/output_DataFrame.csv".')
 
     print('Thanks for running this script. Bye.')
 
